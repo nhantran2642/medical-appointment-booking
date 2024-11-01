ed61b531b2c48441529c8fab3935cb8a0af0bb6c
ldhuy1305 <bongmaxuyentuong1@gmail.com>
2024-11-01 22:42:14 +0700

    config urls

--- be/src/api/settings.py
+++ be/src/api/settings.py
@@ -178,9 +178,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
