e041f4a6c031bbfe8f9a68718672f9626bc81745
ldhuy1305 <bongmaxuyentuong1@gmail.com>
2024-11-02 11:10:30 +0700

    config urls

--- be/src/api/urls.py
+++ be/src/api/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from api import settings
 from django.conf.urls import include
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
