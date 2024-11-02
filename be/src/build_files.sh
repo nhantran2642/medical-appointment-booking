115abfe975dcfc18b1c2e1a4077c8163d5926b94
ldhuy1305 <bongmaxuyentuong1@gmail.com>
2024-11-02 11:11:32 +0700

    config urls

--- be/src/build_files.sh
+++ be/src/build_files.sh
@@ -1,7 +1,7 @@
 pip3 install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
-# Kiểm tra thư mục staticfiles
-if [ ! -d "static" ]; then
-    echo "Error: Directory 'static' was not created."
+
+if [ ! -d "staticfiles" ]; then
+    echo "Error: Directory 'staticfiles' was not created."
     exit 1
 fi
