9ba3954bf70b649926141fe4d2421cffe93ff384
ldhuy1305 <bongmaxuyentuong1@gmail.com>
2024-11-27 23:41:43 +0700

    calc revenue for admin

--- be/src/appointment/serializers.py
+++ be/src/appointment/serializers.py
@@ -1,7 +1,11 @@
+from datetime import timedelta, timezone
+
+from authentication.serializers import UserSerializer
 from doctor.models import Doctor
+from doctor.serializers import DoctorSerializer
 from rest_framework import serializers
 from rest_framework.exceptions import NotFound
-from datetime import timezone, timedelta
+
 from .models import Appointment
 
 
@@ -23,3 +27,37 @@ class AppointmentSerializer(serializers.ModelSerializer):
             **validated_data, user_id=user_id, doctor=doctor
         )
         return instance
+
+
+class AppointmentDetailSerializer(serializers.ModelSerializer):
+    doctor = DoctorSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Appointment
+        fields = "__all__"
+
+
+class ListAppointmentSerializer(serializers.ModelSerializer):
+    list = AppointmentDetailSerializer(many=True)
+
+    class Meta:
+        model = Appointment
+        fields = ("id", "appointment_date", "status", "doctor_id")
+
+    def create(self, validated_data):
+        user_id = self.context["user_id"]
+        try:
+            doctor_id = validated_data.pop("doctor_id")
+            doctor = Doctor.objects.get(pk=doctor_id)
+        except Exception as ex:
+            raise NotFound("Doctor does not exist.")
+        instance = Appointment.objects.create(
+            **validated_data, user_id=user_id, doctor=doctor
+        )
+        return instance
+
+
+class StatSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Appointment
