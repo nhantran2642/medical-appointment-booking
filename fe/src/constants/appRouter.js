82a9211a7d367ecf79edd7b475fe92fee1b18731
Salilyyyy <hgiang140302@gmail.com>
2024-11-27 23:06:57 +0700

    finish

--- fe/src/constants/appRouter.js
+++ fe/src/constants/appRouter.js
@@ -1,10 +1,8 @@
 export const APP_ROUTER = {
     HOME: '/home',
     SERVICE: '/service',
-    DOCTOR: '/doctor',
     SINGLEDOCTOR: '/singledoctor',
     CONTACT: '/contact',
-    BLOG: '/blogspage',
     LOGIN: '/login',
     BOOKAPOINT: '/bookappointment',
     PROFILE: '/profile',
